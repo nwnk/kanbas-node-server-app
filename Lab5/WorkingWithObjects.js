3eefa253244764b15f2190a55fa967a2f7a3279a
Adam Jackson <ajax@nwnk.net>
2024-08-17 21:06:13 -0400

    a5: through 3.3.4

--- Lab5/WorkingWithObjects.js
+++ Lab5/WorkingWithObjects.js
@@ -3,6 +3,14 @@ const assignment = {
   description: "Create a NodeJS server with ExpressJS",
   due: "2021-10-10", completed: false, score: 0,
 };
+
+const module = {
+  id: 1,
+  name: "My Name Is Jonas",
+  description: "Building's not going as he planned; the foreman has injured his hand.",
+  course: "The Blue Album",
+};
+
 export default function WorkingWithObjects(app) {
   app.get("/lab5/assignment", (req, res) => {
     res.json(assignment);
@@ -14,6 +22,32 @@ export default function WorkingWithObjects(app) {
     const { newTitle } = req.params;
     assignment.title = newTitle;
     res.json(assignment);
+  });
+  app.get("/lab5/assignment/score/:newScore", (req, res) => {
+    const { newScore } = req.params;
+    assignment.score = newScore;
+    res.json(assignment);
+  });
+  app.get("/lab5/assignment/completed/:completed", (req, res) => {
+    const { completed } = req.params;
+    assignment.completed = completed
+    res.json(assignment);
+  });
+  app.get("/lab5/module", (req, res) => {
+    res.json(module);
+  });
+  app.get("/lab5/module/name", (req, res) => {
+    res.json(module.name);
+  });
+  app.get("/lab5/module/name/:newName", (req, res) => {
+    const { newName } = req.params;
+    module.name = newName;
+    res.json(module);
+  });
+  app.get("/lab5/module/desc/:newDesc", (req, res) => {
+    const { newDesc } = req.params;
+    module.desc = newDesc;
+    res.json(module);
   });
 
 };
