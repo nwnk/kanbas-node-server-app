a21e1c2e8674e5ca10fcb94cfd9a38bef7f2367c
Adam Jackson <ajax@nwnk.net>
2024-09-22 15:09:11 -0400

    lab5 something or other

---
+++ Kanbas/Database/assignments.js
@@ -0,0 +1,11 @@
+export default [
+  { "_id": "A101", "name": "Propulsion Assignment", "course": "RS101" },
+  { "_id": "A102", "name": "Combustion Analysis", "course": "RS101" },
+  { "_id": "A103", "name": "Nozzle Design Project", "course": "RS101" },
+  { "_id": "A201", "name": "Aerodynamics Quiz", "course": "RS102" },
+  { "_id": "A202", "name": "Flow Analysis", "course": "RS102" },
+  { "_id": "A203", "name": "Heating Analysis", "course": "RS102" },
+  { "_id": "A301", "name": "Structural Design Task", "course": "RS103" },
+  { "_id": "A302", "name": "Orbital Calculations", "course": "RS103" },
+  { "_id": "A303", "name": "Systems Engineering Exam", "course": "RS103" }
+]
